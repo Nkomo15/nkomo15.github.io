0a36d2fae964e22943aa7c0f875e4bfd96e26bc6
Nkomo15 <danecowan15@gmail.com>
2026-02-11 18:55:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "PASTE_YOURS",
+  authDomain: "PASTE_YOURS",
+  databaseURL: "PASTE_YOURS",
+  projectId: "PASTE_YOURS",
+  storageBucket: "PASTE_YOURS",
+  messagingSenderId: "PASTE_YOURS",
+  appId: "PASTE_YOURS"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const chatRef = ref(database, "traitorsChat");
+
 const SECRET_PASSWORD = "traitor";
 
-function enterSecret() {
+window.enterSecret = function() {
     const password = prompt("Enter the Traitors password:");
 
     if (password === SECRET_PASSWORD) {
@@ -8,20 +25,29 @@ function enterSecret() {
     } else {
         alert("You are not worthy... Faithful.");
     }
-}
+};
 
-function sendMessage() {
+window.sendMessage = function() {
     const username = document.getElementById("username").value;
     const message = document.getElementById("message").value;
 
     if (username === "" || message === "") return;
 
-    const chatBox = document.getElementById("chatBox");
-
-    const newMessage = document.createElement("p");
-    newMessage.innerHTML = "<strong>" + username + ":</strong> " + message;
-
-    chatBox.appendChild(newMessage);
+    push(chatRef, {
+        user: username,
+        text: message
+    });
 
     document.getElementById("message").value = "";
-}
+};
+
+onChildAdded(chatRef, (data) => {
+    const chatBox = document.getElementById("chatBox");
+    const msg = data.val();
+
+    const newMessage = document.createElement("p");
+    newMessage.innerHTML = "<strong>" + msg.user + ":</strong> " + msg.text;
+
+    chatBox.appendChild(newMessage);
+});
+
